f407bc4ed23cdc7dd839924147048caee7079636
khaiminh <minhjaka227@gmail.com>
2025-04-25 19:29:06 +0700

    fixing api

--- API/cors.php
+++ API/cors.php
@@ -2,7 +2,7 @@
 // Allow from specific origin
 $allowed_origin = 'https://quanlydiemhocsinhstu.netlify.app';
 
-// Always set these headers
+// Set CORS headers
 header("Access-Control-Allow-Origin: $allowed_origin");
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Max-Age: 86400');    // cache for 1 day
@@ -12,14 +12,6 @@ header('Content-Type: application/json; charset=UTF-8');
 
 // Handle preflight requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    }
-    
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    }
-    
     http_response_code(200);
     exit(0);
 }
